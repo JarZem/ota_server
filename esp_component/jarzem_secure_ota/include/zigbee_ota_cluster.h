d0b140e8cfe9b8ce97a5dbb4fcea486c025ff2b2
JarZem <79940171+JarZem@users.noreply.github.com>
2026-08-21 21:34:30 +0200

    Move secure OTA Zigbee transport interface into component

---
+++ esp_component/jarzem_secure_ota/include/zigbee_ota_cluster.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include "esp_err.h"
+#include "esp_zigbee_attribute.h"
+#include "esp_zigbee_cluster.h"
+#include "zcl/esp_zigbee_zcl_command.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ZIGBEE_OTA_CLUSTER_ID          0xfc00
+#define ZIGBEE_OTA_MANUFACTURER_CODE   0x1234
+#define ZIGBEE_OTA_CONFIG_ATTR_ID      0x0001
+#define ZIGBEE_OTA_ENDPOINT            10
+#define ZIGBEE_OTA_CMD_TO_DEVICE_ID    0x04
+#define ZIGBEE_OTA_CMD_FROM_DEVICE_ID  0x11
+
+esp_err_t zigbee_ota_cluster_add_attrs(esp_zb_attribute_list_t *cluster);
+bool zigbee_ota_cluster_handle_set_attr(const esp_zb_zcl_set_attr_value_message_t *message);
+bool zigbee_ota_cluster_handle_custom_cmd(const esp_zb_zcl_custom_cluster_command_message_t *message);
+void zigbee_ota_schedule_hello(uint32_t delay_ms);
+esp_err_t zigbee_ota_publish_control_state(bool enabled, uint8_t status);
+esp_err_t zigbee_ota_report_download_complete(void);
+
+#ifdef __cplusplus
+}
+#endif
