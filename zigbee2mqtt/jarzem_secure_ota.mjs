cfcb42946191624d53600f7570ef3b889d55a6d1
JarZem <79940171+JarZem@users.noreply.github.com>
2026-08-22 21:03:52 +0200

    Force OTA enable entity writable and enabled by default

--- zigbee2mqtt/jarzem_secure_ota.mjs
+++ zigbee2mqtt/jarzem_secure_ota.mjs
@@ -155,7 +155,7 @@ export const extend=[
 export const fromZigbee=[fromCommand,fromRaw,fromEnable,fromStatus];
 export const toZigbee=[toCommand,toEnable,getStatus];
 export const exposes=[
-    e.binary('enable_ota',ea.ALL,'ON','OFF'),
+    e.binary('enable_ota',ea.ALL,'ON','OFF').withCategory('config').withHomeAssistant({enabledByDefault:true}),
     e.text('ota_status',ea.STATE_GET),
 ];
 export const endpointMap={};
